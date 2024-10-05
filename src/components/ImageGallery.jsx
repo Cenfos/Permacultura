9461ac1abe252fb65ef0e86bc8136507e2e3f2c3
Cenfos <camilo.rguez@gmail.com>
2024-10-05 19:49:44 +0200

    Ajuste realizados en general

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,22 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
-
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-const images = [
-  { src: '/images/permacultura1.jpg', alt: 'Permacultura 1', text: 'Diseño de huerto ecológico' },
-  { src: '/images/permacultura2.jpg', alt: 'Permacultura 2', text: 'Compostaje natural' },
-  { src: '/images/permacultura3.jpg', alt: 'Permacultura 3', text: 'Agroforestería' },
-  { src: '/images/permacultura4.jpg', alt: 'Permacultura 4', text: 'Cultivo en terrazas' },
-];
-
-const ImageGallery = () => {
+function ImageGallery({ images }) {
   return (
-    <div className="image-gallery">
+    <div className="image-gallery max-w-3xl mx-auto">
       <Swiper
         modules={[Navigation, Pagination, Autoplay]}
         spaceBetween={30}
@@ -25,18 +17,37 @@ const ImageGallery = () => {
         pagination={{ clickable: true }}
         autoplay={{ delay: 5000, disableOnInteraction: false }}
         loop={true}
+        className="rounded-lg overflow-hidden"
       >
         {images.map((image, index) => (
           <SwiperSlide key={index}>
-            <div className="slide-content">
-              <img src={image.src} alt={image.alt} />
-              {image.text && <p className="slide-text">{image.text}</p>}
+            <div className="relative aspect-video">
+              <img 
+                src={image.src} 
+                alt={image.alt} 
+                className="object-cover w-full h-full"
+              />
+              {image.text && (
+                <p className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white p-2 text-center">
+                  {image.text}
+                </p>
+              )}
             </div>
           </SwiperSlide>
         ))}
       </Swiper>
     </div>
   );
+}
+
+ImageGallery.propTypes = {
+  images: PropTypes.arrayOf(
+    PropTypes.shape({
+      src: PropTypes.string.isRequired,
+      alt: PropTypes.string.isRequired,
+      text: PropTypes.string
+    })
+  ).isRequired
 };
 
 export default ImageGallery;
