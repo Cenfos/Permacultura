5cbddb1f6200a32a8b524c527e46c3a4fec1a429
Cenfos <camilo.rguez@gmail.com>
2024-10-06 13:43:06 +0200

    Cambios en errores REact solucionados

--- tina/__generated__/config.prebuild.jsx
+++ tina/__generated__/config.prebuild.jsx
@@ -1,18 +1,20 @@
+// tina/config.ts
 import { defineConfig } from "tinacms";
-
-export default defineConfig({
+var config_default = defineConfig({
   branch: "main",
-  clientId: "e99646fb-1133-4d5b-8c62-ec401a155fa1", // Reemplaza con tu ID de cliente de Tina
-  token: "539cb3a2a9cef3b0ad29e85265f1645918ba6d8f", // Reemplaza con tu token de Tina
+  clientId: "e99646fb-1133-4d5b-8c62-ec401a155fa1",
+  // Reemplaza con tu ID de cliente de Tina
+  token: "539cb3a2a9cef3b0ad29e85265f1645918ba6d8f",
+  // Reemplaza con tu token de Tina
   build: {
     outputFolder: "admin",
-    publicFolder: "public",
+    publicFolder: "public"
   },
   media: {
     tina: {
       mediaRoot: "images",
-      publicFolder: "public",
-    },
+      publicFolder: "public"
+    }
   },
   schema: {
     collections: [
@@ -26,16 +28,19 @@ export default defineConfig({
             name: "title",
             label: "Title",
             isTitle: true,
-            required: true,
+            required: true
           },
           {
             type: "rich-text",
             name: "body",
             label: "Body",
-            isBody: true,
-          },
-        ],
-      },
-    ],
-  },
+            isBody: true
+          }
+        ]
+      }
+    ]
+  }
 });
+export {
+  config_default as default
+};
